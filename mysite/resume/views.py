20cfd9171d31db2561edfbe455da33a22fabb196
pandenandini88 <nandinimp@gmail.com>
2022-06-17 18:46:40 -0400

    Added a resume page!

--- mysite/resume/views.py
+++ mysite/resume/views.py
@@ -5,3 +5,6 @@ from django.http import HttpResponse
 
 def index(request):
     return HttpResponse("<h1> Hello World </h1>")
+
+def resume(request):
+    return HttpResponse("<h1> This is the resume page. </h1>")
