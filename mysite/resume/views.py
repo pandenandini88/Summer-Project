32f62340951aa0a1c6b1d2630a4d847eb73045fe
pandenandini88 <nandinimp@gmail.com>
2022-06-18 20:13:13 -0400

    Succesfully added a menu to the home page

--- mysite/resume/views.py
+++ mysite/resume/views.py
@@ -5,9 +5,10 @@ from django.http import HttpResponse
 # Create your views here.
 
 def index(request):
-    htmlString = "<h1>Some Title</h1><a href = 'http://127.0.0.1:8000/resume/'>Resume Page</a>"
-    return HttpResponse(htmlString)
+    htmlLinks = "<pre><a href = 'http://127.0.0.1:8000'>Home Page</a>      <a href = 'http://127.0.0.1:8000/resume/'>Resume Page</a></pre>"
+    htmlTitle = "<center><h1> Home Page </h1></center>"
+    return HttpResponse(htmlLinks + htmlTitle)
     
 
 def resume(request):
-    return HttpResponse("<h1> This is the resume page. </h1>")
+    return HttpResponse("<center><h1> This is the resume page. </h1></center>")
