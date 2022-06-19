06c0aebc59e4a9a6da68663a0b37f6c2e3bbcc0d
pandenandini88 <nandinimp@gmail.com>
2022-06-18 20:16:34 -0400

    Added menu to resume page and added resume page to menu

--- mysite/resume/views.py
+++ mysite/resume/views.py
@@ -4,11 +4,13 @@ from django.http import HttpResponse
 
 # Create your views here.
 
+htmlLinks = "<pre><a href = 'http://127.0.0.1:8000'>Home Page</a>      <a href = 'http://127.0.0.1:8000/resume/'>Resume Page</a></pre>"
+
 def index(request):
-    htmlLinks = "<pre><a href = 'http://127.0.0.1:8000'>Home Page</a>      <a href = 'http://127.0.0.1:8000/resume/'>Resume Page</a></pre>"
-    htmlTitle = "<center><h1> Home Page </h1></center>"
-    return HttpResponse(htmlLinks + htmlTitle)
+    htmlHomeTitle = "<center><h1> Home Page </h1></center>"
+    return HttpResponse(htmlLinks + htmlHomeTitle)
     
 
 def resume(request):
-    return HttpResponse("<center><h1> This is the resume page. </h1></center>")
+    htmlResumeTitle = "<center><h1> Resume Page </h1></center>"
+    return HttpResponse(htmlLinks + htmlResumeTitle)
