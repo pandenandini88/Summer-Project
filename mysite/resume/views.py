f9ae4cb094cadc389e622b2f4a2f2e58eed00dc9
pandenandini88 <nandinimp@gmail.com>
2022-06-18 20:02:01 -0400

    Added code that linked the two pages

--- mysite/resume/views.py
+++ mysite/resume/views.py
@@ -1,10 +1,13 @@
+import re
 from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
 
 def index(request):
-    return HttpResponse("<h1> Hello World </h1>")
+    htmlString = "<h1>Some Title</h1><a href = 'http://127.0.0.1:8000/resume/'>Resume Page</a>"
+    return HttpResponse(htmlString)
+    
 
 def resume(request):
     return HttpResponse("<h1> This is the resume page. </h1>")
