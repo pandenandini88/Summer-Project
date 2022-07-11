ed2faca1ebb7624e692d19fe00251290529a4ea4
pandenandini88 <nandinimp@gmail.com>
2022-07-11 01:28:21 -0400

    Fixed a bug where clicking on the home page on the menu redirected to polls page

--- mysite/polls/views.py
+++ mysite/polls/views.py
@@ -2,6 +2,6 @@ from django.shortcuts import render
 from django.http import HttpResponse
 
 # Create your views here.
-def index(request):
-    return HttpResponse("Hello, world. You're at the polls index.")
+def poll_index(request):
+    return render(request, polls.html)
 
