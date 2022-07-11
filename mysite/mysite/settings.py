8f182c7ac68be1cc23690787670debe0afcddfe6
pandenandini88 <nandinimp@gmail.com>
2022-07-11 01:22:46 -0400

    Downloaded dontenv to help keep the secret key private, think it worked.

--- mysite/mysite/settings.py
+++ mysite/mysite/settings.py
@@ -10,7 +10,12 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/4.0/ref/settings/
 """
 
+import os
 from pathlib import Path
+from dotenv import load_dotenv
+
+load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -20,7 +25,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-f6)b*gbrdi=r_!0kec9*dp2a%l0f(!1t34_+68c5xyefiv3ya+'
+SECRET_KEY = str(os.getenv('SECRET_KEY'))
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
