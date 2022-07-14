e6108ff31fa44db1476d466de77e1df6746f31d2
ducoterra <ducoterra@gmail.com>
2022-07-14 10:50:28 -0400

    Format views.py with python black
    Use python black to format views.py. Always format before you commit
    your code.

--- mysite/resume/views.py
+++ mysite/resume/views.py
@@ -4,7 +4,7 @@ from django.http import HttpResponse
 
 # Create your views here.
 
-# This function takes a request from the user and displays elements on to the home page. home.html can be found under the templates folder.
+
 def index(request):
     """
     This function takes a request from the user and displays elements on to the home page.
@@ -17,9 +17,11 @@ def index(request):
         render: A template to render
     """
     return render(request, "home.html")
-    
+
 
 # This function takes a request from the user and displays elements on to the resume page. resume.html can be found under the templates folder.
 def resume(request):
     return render(request, "resume.html")
+
+
 # This function takes a request from the user and displays elements on to the polls page. polls.html can be found under the templates folder.
