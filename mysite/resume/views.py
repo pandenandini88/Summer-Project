7c8e2dd4ae64312cd63f1a4f51639d84ea966f16
ducoterra <ducoterra@gmail.com>
2022-07-14 10:49:37 -0400

    Add example docstring comment under views.py
    Provide an example docstring comment created with autodocstring (vscode
    extension). This should be used as an example to create more docstring
    comments.

--- mysite/resume/views.py
+++ mysite/resume/views.py
@@ -6,6 +6,16 @@ from django.http import HttpResponse
 
 # This function takes a request from the user and displays elements on to the home page. home.html can be found under the templates folder.
 def index(request):
+    """
+    This function takes a request from the user and displays elements on to the home page.
+    home.html can be found under the templates folder.
+
+    Args:
+        request (Django Request): A Django Request
+
+    Returns:
+        render: A template to render
+    """
     return render(request, "home.html")
     
 
