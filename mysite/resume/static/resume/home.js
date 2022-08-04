698ded039afc00df15363c443f614bc5538b76c9
pandenandini88 <nandinimp@gmail.com>
2022-08-04 14:14:24 -0400

    Fixes for problems

--- mysite/resume/static/resume/home.js
+++ mysite/resume/static/resume/home.js
@@ -5,3 +5,20 @@ let change_color = async function() {
 }
 
 setInterval(change_color, 100);
+
+//starts confetti
+const start = () => {
+    setTimeout(function(){
+      confetti.start();
+    }, 1000); //indicates time, so 1 second
+  };
+
+  //stops confetti
+  const stop = () => {
+    setTimeout(function(){
+      confetti.stop()
+    }, 5000) //indicates time, so 5 seconds
+  }
+
+  start();
+  stop();
